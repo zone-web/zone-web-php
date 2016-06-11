69ede31cb73d83e94eddc5495daf4084bcd414a7
apple <apple@appledeMacBook-Air.local>
2016-06-11 22:16:03 +0800

    add slider

---
+++ slider.php
@@ -0,0 +1,24 @@
+<?php include './header.php'; ?>
+
+<title>详情</title>
+<link rel="stylesheet" type="text/css" href="css/slider.css">
+<div class="slider-container">
+	<div class="slider-content">
+		<div class="img-wrapper">
+			<img src="images/1.jpg" alt="">
+			<div class="desc">
+				<h1>现代风精品小户型</h1>
+				<p>设计师通过各个空间的不同材质的重叠使用，打造空间的多重样貌，创造出小户型空间的多重利用价值。</p>
+			</div>
+		</div>
+		<div class="img-wrapper">
+			<img src="images/2.jpg" alt="">
+		</div>
+		<div class="img-wrapper">
+			<img src="images/3.jpg" alt="">
+		</div>
+	</div>
+</div>
+
+<?php include './footer.php'; ?>
+
